46dd85f1402b5b67f8c9976efaad2b4b0ae4e92d
bilal-mustafa10 <bilalmustafa041@icloud.com>
2023-03-10 20:40:38 +0000

    initial commit

---
+++ mindmateDashboard/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2@mo%qah-q-@o+rij)nc%ydc+6!anrlqy#+yme93@s*2wh+o@t"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
